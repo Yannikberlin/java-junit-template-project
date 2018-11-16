4603ccead68996b66422384e1efed31788a8e693
yannikdumke <yannikdumke@10.211.55.3>
2018-11-16 13:26:43 +0100

    Zurück

--- test/helloWorld/Klasse.java
+++ test/helloWorld/Klasse.java
@@ -3,7 +3,19 @@ package helloWorld;
 import java.util.*;
 
 public class Klasse {
-
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Zahl 1: ");
+		double zahl1 = sc.nextInt();
+		System.out.println("Zahl 2: ");
+		double zahl2 = sc.nextInt();
+		
+		Klasse obj = new Klasse();
+		double erg = obj.addiere(zahl1,zahl2);
+		System.out.println(erg);
+		
+		
+	}
 	public double addiere(double x, double y) {
 		double erg = x+y;
 		return erg;
